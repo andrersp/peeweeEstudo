5e07709c5fde0219c33834e4098b22c2b7c42cd7
andrersp <rsp.assistencia@gmail.com>
2019-02-21 03:12:45 -0300

    novas Classes

--- Crud/Conexao.py
+++ Crud/Conexao.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-from peewee import MySQLDatabase
+from peewee import Model
+from peewee import DatabaseError
 from peewee import IntegerField
 from peewee import ForeignKeyField
 from peewee import DecimalField
 from peewee import BigBitField
 from peewee import CharField
 from peewee import PrimaryKeyField
-from peewee import DatabaseError
-from peewee import Model
+from peewee import DateField
 from playhouse.mysql_ext import MySQLConnectorDatabase
 
 
@@ -49,6 +49,8 @@ class LongBlobCampo(BigBitField):
 
 
 # Base model
+
+
 class BaseModel(Model):
     class Meta:
         conecta = Conexao()
@@ -116,7 +118,6 @@ class Cliente(BaseModel):
 
 
 #  Tabela Fornecedor
-
 class Fornecedor(BaseModel):
     id = PrimaryKeyField(null=False)
     nome_fantasia = CharField(max_length=80)
@@ -136,3 +137,101 @@ class Fornecedor(BaseModel):
 
     class Meta:
         db_table = 'fornecedor'
+
+
+# Tabela Categoria a pagar
+class CatAPagar(BaseModel):
+    id = PrimaryKeyField(null=False)
+    categoria_a_pagar = CharField(max_length=80)
+
+    class Meta:
+        db_table = 'categoria_a_pagar'
+
+
+# Tabela Categoria a Receber
+class CatAReceber(BaseModel):
+    id = PrimaryKeyField(null=False)
+    categoria_a_receber = CharField(max_length=80)
+
+    class Meta:
+        db_table = 'categoria_a_receber'
+
+
+# Tabela Forma de Pagamento
+class FormaPagamento(BaseModel):
+    id = PrimaryKeyField(null=False)
+    forma_pagamento = CharField(max_length=80)
+
+    class Meta:
+        db_table = 'forma_de_pagamento'
+
+
+# Tabela Status Pagamento
+class StatusPagamento(BaseModel):
+    id = PrimaryKeyField(null=False)
+    status_pagamento = CharField(max_length=80)
+
+    class Meta:
+        db_table = 'status_pagamento'
+
+
+# Tabela Status Entrega
+class StatusEntrega(BaseModel):
+    id = PrimaryKeyField(null=False)
+    status_entrega = CharField(max_length=80)
+
+    class Meta:
+        db_table = 'status_entrega'
+
+
+# Tabela Contas a Pagar
+class ContaAPagar(BaseModel):
+    id = PrimaryKeyField(null=False)
+    id_compra = IntegerField(null=True, default=0)
+    id_fornecedor = ForeignKeyField(Fornecedor, column_name='id_fornecedor')
+    descricao = CharField(max_length=150)
+    obs = CharField(max_length=150)
+    categoria = ForeignKeyField(CatAPagar, column_name='categoria')
+    data_vencimento = DateField()
+    valor = DecimalField(9, 2)
+    forma_pagamento = IntegerField(null=True)
+    data_pagamento = DateField()
+    valor_pago = DecimalField(9, 2)
+    status = IntegerField(null=False)
+
+    class Meta:
+        db_table = 'conta_a_pagar'
+
+
+# Tabela Contas a Receber
+class ContaAReceber(BaseModel):
+    id = PrimaryKeyField(null=False)
+    id_venda = IntegerField(null=True, default=0)
+    id_cliente = ForeignKeyField(Cliente, column_name='id_cliente')
+    descricao = CharField(max_length=150)
+    obs = CharField(max_length=150)
+    categoria = ForeignKeyField(CatAReceber, column_name='categoria')
+    data_vencimento = DateField()
+    valor = DecimalField(9, 2)
+    forma_pagamento = IntegerField(null=True)
+    data_recebimento = DateField()
+    valor_recebido = DecimalField(9, 2)
+    status = IntegerField(null=False)
+
+    class Meta:
+        db_table = 'conta_a_receber'
+
+
+Conexao()
+CategoriaProduto.create_table()
+MarcaProduto.create_table()
+Produto.create_table()
+Fornecedor.create_table()
+Cliente.create_table()
+CatAPagar.create_table()
+CatAReceber.create_table()
+StatusEntrega.create_table()
+StatusPagamento.create_table()
+FormaPagamento.create_table()
+ContaAPagar.create_table()
+ContaAReceber.create_table()
